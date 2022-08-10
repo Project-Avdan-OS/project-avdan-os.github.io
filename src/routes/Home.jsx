a2df68a7ad6984bbb804c6468f8b5398e9be547a
Deknoinarak - Nonthapat Jumjai <chitah09@gmail.com>
2022-08-10 23:51:56 +0700

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -163,7 +163,36 @@ export const Home = () => {
         </div>
       </div>
 
-      <div className="bg-gray-800 flex justify-between items-center py-5 rounded-md shadow">
+      <div className="bg-gradient-to-r from-blue-600 via-violet-700 to-rose-600">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-3 w-full h-screen place-items-center gap-5">
+            <div className="col-span-2">
+              <span className="text-7xl font-bold">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 via-purple-400 to-sky-400">
+                  AvdanOS
+                </span>{" "}
+                is under development
+              </span>
+              <p className="text-sm text-slate-400 mt-5 mb-10">
+                If you are a developer or a designer then please join us to make
+                it real. Even if you're not, join us to show your support.
+              </p>
+            </div>
+
+            <div className="col-span-1">
+              <a
+                href="https://discord.io/avdanos"
+                className="bg-gradient-to-r bg-size-200 bg-pos-0 text-2xl from-indigo-600 to-indigo-800 py-5 font-medium rounded-md hover:bg-pos-100 transition-all duration-150 px-10 cursor-pointer"
+              >
+                <i className="fa-brands fa-discord mr-2"></i> Join Discord For
+                Updates!
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="bg-gray-800 flex justify-between items-center py-5 rounded-md shadow">
         <div className="mx-8 font-bold text-2xl">
           Interested? Give it a try!
         </div>
@@ -182,7 +211,7 @@ export const Home = () => {
             <i className="fa-solid fa-download mr-2"></i> Download Now!
           </a>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
